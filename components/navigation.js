58b1b9719e001b5f160583b9b02bd89e39d37447
Yannick Bättig <yannick.baettig@gmail.com>
2021-03-31 19:49:53 +0200

    navbar

--- components/navigation.js
+++ components/navigation.js
@@ -1,45 +1,73 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navigation = () => {
+  const router = useRouter();
   return (
-    <div className="flex">
-      <div>
+    <div className="flex relative">
+      <div className="p-5 absolute">
         <Link href="/">
           <a>
             <img src="/img/logo.png" />
           </a>
         </Link>
       </div>
-      <ul className="flex justify-center items-center space-x-8 mx-auto">
+      <ul className="flex justify-center items-center space-x-8 p-5 mx-auto h-[100px]">
         <li>
           <Link href="/">
-            <a>Home</a>
+            <a className={router.pathname == "/" ? "text-green-600" : ""}>
+              Home
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <a>Über uns</a>
+            <a className={router.pathname == "/about" ? "text-green-600" : ""}>
+              Über uns
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/education">
-            <a>Ausbildung</a>
+            <a
+              className={
+                router.pathname == "/education" ? "text-green-600" : ""
+              }
+            >
+              Ausbildung
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/posts">
-            <a>Beiträge</a>
+            <a
+              className={
+                router.pathname.startsWith("/posts") ? "text-green-600" : ""
+              }
+            >
+              Beiträge
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/events">
-            <a>Events</a>
+            <a
+              className={
+                router.pathname.startsWith("/events") ? "text-green-600" : ""
+              }
+            >
+              Events
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/contact">
-            <a>Kontakt</a>
+            <a
+              className={router.pathname == "/contact" ? "text-green-600" : ""}
+            >
+              Kontakt
+            </a>
           </Link>
         </li>
       </ul>
